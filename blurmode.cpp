231998dc310f629692ce77a4b3720df9d8604137
sepera_okeq <54331959+Sepera-okeq@users.noreply.github.com>
2022-09-26 04:55:41 +0300

    [ANY] Add code  (ver. 0.9.0)

---
+++ blurmode.cpp
@@ -0,0 +1,165 @@
+#include "blurmode.h"
+
+#include <QDebug>
+
+BlurMode::BlurMode() {}
+
+BlurMode::~BlurMode() {}
+
+RTL_OSVERSIONINFOW BlurMode::GetWindowsVersion() {
+  HMODULE hmodule = ::GetModuleHandleW(L"ntdll.dll");
+  if (hmodule) {
+    RtlGetVersionPtr rtl_get_version_ptr =
+        (RtlGetVersionPtr)::GetProcAddress(hmodule, "RtlGetVersion");
+    if (rtl_get_version_ptr != nullptr) {
+      RTL_OSVERSIONINFOW rovi = {0};
+      rovi.dwOSVersionInfoSize = sizeof(rovi);
+      if (STATUS_SUCCESS == rtl_get_version_ptr(&rovi)) {
+        return rovi;
+      }
+    }
+  }
+  RTL_OSVERSIONINFOW rovi = {0};
+  return rovi;
+}
+
+HWND BlurMode::GetParentWindow(QMainWindow *window) {
+  return reinterpret_cast<HWND>(window->winId());
+}
+
+void BlurMode::HandleMethodCall(QMainWindow *window, int effect, BlurModeEdit mode, bool dark) {
+  if (mode == BlurModeEdit::kInitialize) {
+    if (!is_initialized_) {
+      user32 = ::GetModuleHandleA("user32.dll");
+      if (user32) {
+        set_window_composition_attribute_ =
+            reinterpret_cast<SetWindowCompositionAttribute>(
+                ::GetProcAddress(user32, "SetWindowCompositionAttribute"));
+        if (set_window_composition_attribute_) {
+          is_initialized_ = true;
+          //result->Success();
+          qDebug() << "Success";
+        } else
+          //result->Error("-2", "FAIL_LOAD_METHOD");
+          qDebug() << "FAIL_LOAD_METHOD";
+      } else
+        //result->Error("-1", "FAIL_LOAD_DLL");
+        qDebug() << "FAIL_LOAD_DLL";
+    } else
+      //result->Success();
+      qDebug() << "Success";
+  } else if (mode == kSetEffect) {
+    // Set [ACCENT_DISABLED] as [ACCENT_POLICY] in
+    // [SetWindowCompositionAttribute] to apply styles properly.
+    ACCENT_POLICY accent = {ACCENT_DISABLED, 2, static_cast<DWORD>(0), 0};
+    WINDOWCOMPOSITIONATTRIBDATA data;
+    data.Attrib = WCA_ACCENT_POLICY;
+    data.pvData = &accent;
+    data.cbData = sizeof(accent);
+    set_window_composition_attribute_(BlurMode::GetParentWindow(window), &data);
+    // Only on later Windows 11 versions and if effect is WindowEffect.mica,
+    // WindowEffect.acrylic or WindowEffect.tabbed, otherwise fallback to old
+    // approach.
+    if (GetWindowsVersion().dwBuildNumber >= 22523 && effect > 3) {
+      BOOL enable = TRUE, dark_bool = dark;
+      MARGINS margins = {-1};
+      ::DwmExtendFrameIntoClientArea(BlurMode::GetParentWindow(window), &margins);
+      ::DwmSetWindowAttribute(BlurMode::GetParentWindow(window), 20, &dark_bool,
+                              sizeof(dark_bool));
+      INT effect_value = effect == 4 ? 3 : effect == 5 ? 2 : 4;
+      ::DwmSetWindowAttribute(BlurMode::GetParentWindow(window), 38, &effect_value,
+                              sizeof(enable));
+    } else {
+      if (effect == 5) {
+        // Check for Windows 11.
+        if (GetWindowsVersion().dwBuildNumber >= 22000) {
+          BOOL enable = TRUE, dark_bool = dark;
+          MARGINS margins = {-1};
+          // Mica effect requires [DwmExtendFrameIntoClientArea & "sheet of
+          // glass"
+          // effect with negative margins.
+          ::DwmExtendFrameIntoClientArea(BlurMode::GetParentWindow(window), &margins);
+          ::DwmSetWindowAttribute(BlurMode::GetParentWindow(window), 20, &dark_bool,
+                                  sizeof(dark_bool));
+          ::DwmSetWindowAttribute(BlurMode::GetParentWindow(window), 1029, &enable,
+                                  sizeof(enable));
+        }
+      } else {
+        // Restore original window style & [DwmExtendFrameIntoClientArea] margin
+        // if the last set effect was [WindowEffect.mica], since it sets
+        // negative margins to the window.
+        if ((GetWindowsVersion().dwBuildNumber >= 22000 &&
+                window_effect_last_ == 5) ||
+            ((GetWindowsVersion().dwBuildNumber >= 22523 &&
+             window_effect_last_ > 3))) {
+          BOOL enable = FALSE;
+          // Atleast one margin should be non-negative in order to show the DWM
+          // window shadow created by handling [WM_NCCALCSIZE].
+          //
+          // Matching value with bitsdojo_window.
+          // https://github.com/bitsdojo/bitsdojo_window/blob/adad0cd40be3d3e12df11d864f18a96a2d0fb4fb/bitsdojo_window_windows/windows/bitsdojo_window.cpp#L149
+          MARGINS margins = {0, 0, 1, 0};
+          ::DwmExtendFrameIntoClientArea(BlurMode::GetParentWindow(window), &margins);
+          ::DwmSetWindowAttribute(BlurMode::GetParentWindow(window), 20, &enable,
+                                  sizeof(enable));
+          ::DwmSetWindowAttribute(BlurMode::GetParentWindow(window), 1029, &enable,
+                                  sizeof(enable));
+        }
+        accent = {
+            static_cast<ACCENT_STATE>(effect), 2,
+            static_cast<DWORD>(
+                (255 << 24) +
+                (0 << 16) +
+                (100 << 8) +
+                (100)),
+            0};
+        data.Attrib = WCA_ACCENT_POLICY;
+        data.pvData = &accent;
+        data.cbData = sizeof(accent);
+        set_window_composition_attribute_(BlurMode::GetParentWindow(window), &data);
+      }
+    }
+    window_effect_last_ = effect;
+    //result->Success();
+  } else if (mode == BlurModeEdit::kHideWindowControls) {
+    ::SetWindowLongW(BlurMode::GetParentWindow(window), GWL_STYLE,
+                    ::GetWindowLongW(BlurMode::GetParentWindow(window), GWL_STYLE) &
+                        (0xFFFFFFFF ^ WS_SYSMENU));
+    //result->Success();
+  } else if (mode == BlurModeEdit::kShowWindowControls) {
+    ::SetWindowLongW(BlurMode::GetParentWindow(window), GWL_STYLE,
+                    ::GetWindowLongW(BlurMode::GetParentWindow(window), GWL_STYLE) | WS_SYSMENU);
+    //result->Success();
+  } /*else if (mode == BlurModeEdit::kEnterFullscreen) {
+    if (!is_fullscreen_) {
+      is_fullscreen_ = true;
+      HWND window = BlurMode::GetParentWindow(window);
+      HMONITOR monitor = ::MonitorFromWindow(window, MONITOR_DEFAULTTONEAREST);
+      MONITORINFO info;
+      info.cbSize = sizeof(MONITORINFO);
+      GetMonitorInfo(monitor, &info);
+      SetWindowLongPtr(window, GWL_STYLE, WS_POPUP | WS_VISIBLE);
+      ::GetWindowRect(window, &last_rect_);
+      ::SetWindowPos(
+          window, HWND_TOPMOST, info.rcMonitor.left, info.rcMonitor.top,
+          info.rcMonitor.right - info.rcMonitor.left,
+          info.rcMonitor.bottom - info.rcMonitor.top, SWP_SHOWWINDOW);
+      ::ShowWindow(window, SW_MAXIMIZE);
+    }
+    //result->Success();
+  } else if (mode == BlurModeEdit::kExitFullscreen) {
+    if (is_fullscreen_) {
+      is_fullscreen_ = false;
+      HWND window = BlurMode::GetParentWindow(window);
+      ::SetWindowLongPtr(window, GWL_STYLE, WS_OVERLAPPEDWINDOW | WS_VISIBLE);
+      ::SetWindowPos(window, HWND_NOTOPMOST, last_rect_.left, last_rect_.top,
+                     last_rect_.right - last_rect_.left,
+                     last_rect_.bottom - last_rect_.top, SWP_SHOWWINDOW);
+      ::ShowWindow(window, SW_RESTORE);
+    }*/
+    //result->Success();
+    qDebug() << "Success";
+  } //else
+    //result->NotImplemented();
+    //qDebug() << "NotImplemented";
+
---
+++ blurmode.h
@@ -0,0 +1,108 @@
+#ifndef BLURMODE_H
+#define BLURMODE_H
+
+#include <thread>
+#include <iostream>
+
+#include <windows.h>
+#include <dwmapi.h>
+#include <VersionHelpers.h>
+
+#include <QObject>
+#include <QMainWindow>
+
+#pragma comment(lib, "dwmapi.lib")
+#pragma comment(lib, "comctl32.lib")
+
+typedef enum _WINDOWCOMPOSITIONATTRIB {
+  WCA_UNDEFINED = 0,
+  WCA_NCRENDERING_ENABLED = 1,
+  WCA_NCRENDERING_POLICY = 2,
+  WCA_TRANSITIONS_FORCEDISABLED = 3,
+  WCA_ALLOW_NCPAINT = 4,
+  WCA_CAPTION_BUTTON_BOUNDS = 5,
+  WCA_NONCLIENT_RTL_LAYOUT = 6,
+  WCA_FORCE_ICONIC_REPRESENTATION = 7,
+  WCA_EXTENDED_FRAME_BOUNDS = 8,
+  WCA_HAS_ICONIC_BITMAP = 9,
+  WCA_THEME_ATTRIBUTES = 10,
+  WCA_NCRENDERING_EXILED = 11,
+  WCA_NCADORNMENTINFO = 12,
+  WCA_EXCLUDED_FROM_LIVEPREVIEW = 13,
+  WCA_VIDEO_OVERLAY_ACTIVE = 14,
+  WCA_FORCE_ACTIVEWINDOW_APPEARANCE = 15,
+  WCA_DISALLOW_PEEK = 16,
+  WCA_CLOAK = 17,
+  WCA_CLOAKED = 18,
+  WCA_ACCENT_POLICY = 19,
+  WCA_FREEZE_REPRESENTATION = 20,
+  WCA_EVER_UNCLOAKED = 21,
+  WCA_VISUAL_OWNER = 22,
+  WCA_HOLOGRAPHIC = 23,
+  WCA_EXCLUDED_FROM_DDA = 24,
+  WCA_PASSIVEUPDATEMODE = 25,
+  WCA_USEDARKMODECOLORS = 26,
+  WCA_LAST = 27
+} WINDOWCOMPOSITIONATTRIB;
+
+typedef struct _WINDOWCOMPOSITIONATTRIBDATA {
+  WINDOWCOMPOSITIONATTRIB Attrib;
+  PVOID pvData;
+  SIZE_T cbData;
+} WINDOWCOMPOSITIONATTRIBDATA;
+
+typedef enum _ACCENT_STATE {
+  ACCENT_DISABLED = 0,
+  ACCENT_ENABLE_GRADIENT = 1,
+  ACCENT_ENABLE_TRANSPARENTGRADIENT = 2,
+  ACCENT_ENABLE_BLURBEHIND = 3,
+  ACCENT_ENABLE_ACRYLICBLURBEHIND = 4,
+  ACCENT_ENABLE_HOSTBACKDROP = 5,
+  ACCENT_INVALID_STATE = 6
+} ACCENT_STATE;
+
+typedef struct _ACCENT_POLICY {
+  ACCENT_STATE AccentState;
+  DWORD AccentFlags;
+  DWORD GradientColor;
+  DWORD AnimationId;
+} ACCENT_POLICY;
+
+typedef BOOL(WINAPI* GetWindowCompositionAttribute)(
+    HWND, WINDOWCOMPOSITIONATTRIBDATA*);
+typedef BOOL(WINAPI* SetWindowCompositionAttribute)(
+    HWND, WINDOWCOMPOSITIONATTRIBDATA*);
+
+typedef LONG NTSTATUS, *PNTSTATUS;
+#define STATUS_SUCCESS (0x00000000)
+
+typedef NTSTATUS(WINAPI* RtlGetVersionPtr)(PRTL_OSVERSIONINFOW);
+
+enum BlurModeEdit {
+    kInitialize,
+    kSetEffect,
+    kHideWindowControls,
+    kShowWindowControls,
+    kEnterFullscreen,
+    kExitFullscreen
+};
+
+class BlurMode {
+ public:
+  BlurMode();
+  virtual ~BlurMode();
+  void HandleMethodCall(QMainWindow *window, int effect, BlurModeEdit mode, bool dark);
+
+ private:
+  HMODULE user32 = nullptr;
+  SetWindowCompositionAttribute set_window_composition_attribute_ = nullptr;
+  bool is_initialized_ = false;
+  bool is_fullscreen_ = false;
+  RECT last_rect_ = {};
+  int32_t window_effect_last_ = 0;
+  RTL_OSVERSIONINFOW GetWindowsVersion();
+  HWND GetParentWindow(QMainWindow *window);
+};
+
+
+#endif // BLURMODE_H
